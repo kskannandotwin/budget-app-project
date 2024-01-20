b69f333b2f3ea6ab8a26621c15573c17da7ae3e8
kskannandotwin <kskannan.win@gmail.com>
2024-01-20 22:33:55 +0530

    20.01.2024-sutfel
    completed

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
 // budget controller
-var budgetController = (function() {
- 
-    var x = 23;
-    
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return(add(b));
-        }
-    }
-    
-})();
+var budgetController = (function () {})();
 
 // ui controller
-var UIController = (function() {   
-
-    
-    
-})();
+var UIController = (function () {})();
 
 // global controller
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Clear the fields
+    // 5. Calculate and update budget
+    // 6. Calculate and update percentages
+    console.log('It Works');
+  };
 
-    var z = budgetCtrl.publicTest(5);
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+        console.log('ENTER was pressed');
     }
-    
+  })
 })(budgetController, UIController);
