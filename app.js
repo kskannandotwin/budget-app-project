9d8dcfa43722bf1b140ca17ed11dc4daf8c896d8
kannanATexcelforte <kannan@excelforte.com>
2024-01-24 09:17:49 +0530

    24.01.2024-sutfel
    completed

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 var budgetController = (function () {
-  var x = 23;
-
-  var add = function (a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function (b) {
-      return(add(b));
-    }
-  }
-})();
-
-var UIController = (function () {
   // some code
 })();
 
-var controller = (function (budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
+var UIController = (function () {
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
 
   return {
-    anotherPublic: function() {
-      console.log(z);
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
     }
   }
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
+
+  var ctrlAddItem = function () {
+    // 1. get the input field data
+    var input = UICtrl.getInput();
+    console.log(input);
+    // 2. add the item to the budget controller
+
+    // 3. add the item to the UI
+
+    // 4. calculate the budget
+
+    // 5. display the budget on the UI
+  }
+
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 
 })(budgetController, UIController);
